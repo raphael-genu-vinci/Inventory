8c8163452867eef28ad2fcc028d45b222d293d58
rvph10 <rgenu10@gmail.com>
2024-05-10 18:55:51 +0200

    first commit

---
+++ data/db_init.sql
@@ -0,0 +1,13 @@
+-- Create the database
+DROP TABLE IF EXISTS ingredient;
+
+CREATE TABLE ingredient (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ingredient_id INTEGER NOT NULL UNIQUE,
+    type TEXT NOT NULL,
+    name TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    expiration_date DATE NOT NULL,
+    in_date DATE NOT NULL,
+);
+
